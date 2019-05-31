8b279cdebb376a11c80f53cb909d4043e5bbe67a
poignant-minifier-0983 <davidfano@gmail.com>
2019-05-31 21:22:41 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,15 +1,17 @@
 class CashRegister
-  attr_accessor :total, :discount, :add_item
+  attr_accessor :total, :discount, :items
 
   def initialize(discount=0)
     @total = 0
     @discount = discount
+    @items = []
   end
 
-  def add_item(title, price, increase)
-    @title = title
-    @price = price
-    @increase = increase
+  def add_item(title, amount, quantity=1)
+    self.total += amount * quantity
+    quantity.times do
+      items << title
+    end
   end
 
 end
